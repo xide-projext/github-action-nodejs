e45a0ff237e07e6fde66e52e825382287bd8af03
Seungwoo hong <1100974+hongsw@users.noreply.github.com>
2023-10-31 16:22:17 +0900

    개발자에게 어플리케이션을 받았다

---
+++ test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('./app');
+
+describe('GET /', () => {
+    it('responds with "Hello, World!"', done => {
+        request(app)
+            .get('/')
+            .expect(200, 'Hello, World!', done);
+    });
+});
